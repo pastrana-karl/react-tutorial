63325f8e35ab8f97d6758c5b68767b2f2fb093ea
Karl <pastranakd@gmail.com>
2021-06-20 12:00:56 +0800

    using json server
    REST API and UI update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,79 @@
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  //Fetch tasks
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+
+  //Fetch task
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
 
   // const name = 'Karl'
   // const x = false
 
+  // Add task function
+
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', { method: 'POST', headers: {'Content-type': 'application/json'}, body: JSON.stringify(task) })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
+  }
+
+  // Delete task function
+
+  const deleteTask = async (id) => {
+    await fetch (`http://localhost:5000/tasks/${id}`, { method: 'DELETE' })
+
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  // Toggle reminder function
+
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, { method: 'PUT', headers: {'Content-type': 'application/json'}, body: JSON.stringify(updatedTask)})
+    const data = await res.json()
+
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task))
+  }
   return (
     <div className="container">
-      <Header />
+      <Header onAdd = {() => setShowAddTask(!showAddTask)} showAdd = {showAddTask}/>
+      {showAddTask && <AddTask onAdd = {addTask}/>}
+      {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder}/> : 'No Tasks To Show...'}
       {/* <Header title={1} />  Testing error handling of prop types */}
       {/* <Header title='Hello'/> Testing props handling */}
       {/* <h1>Hello From React</h1> */}
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text }) => {
+const Button = ({ color, text, onClick }) => {
     return (
-        <button style = {{backgroundColor: color}} className = 'btn'>{text}</button>
+        <button onClick = {onClick} style = {{backgroundColor: color}} className = 'btn'>{text}</button>
     )
 }
 
@@ -13,6 +13,7 @@ Button.defaultProps = {
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 
 export default Button
